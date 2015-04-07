748c4924ba20cc4bc8630bfb99bb98cf09e2d91b
unknown <tonts905@CE105WD554105S.hillnet.ad.hsd.k12.or.us>
2015-04-07 14:53:04 -0700

    Updated

--- template.py
+++ template.py
@@ -22,15 +22,15 @@ def intro():
                         "\nWWIII has happened, resulting in world wide chemical, biological"
                         "\nand nuclear war. You stand in front of a duffle bag of money and"
                         "\na duffle bag of gold riches. Which do you take?")
-    choice = simpledialog.askinteger("Choose wisely","\nPaper Money (1)\nGold Riches (2).")
+    choice = simpledialog.askinteger("Choose wisely","\nGold Riches (1)\nPaper Money (2).")
     if choice == 1:
         choice1()
     elif choice == 2:
-        choice2()
+        choiceMoney1()
     else:
         intro()
 
-################ Brad's Fuctions #####################
+################ Joseph's Fuctions #####################
 def choice1():
     choice = simpledialog.askinteger("Choose wisely",
                                      "This is the next part of the story.  Now you must choose 1 or 2 again.")
@@ -44,34 +44,39 @@ def choice1():
     else:
         choice1()
 
+################ Brad's Functions #####################
+
+""" Continue Joseph's functions """
+
+
 ################ Steven's Functions #####################
-def choice2():
+def choiceMoney1():
+    messagebox.showinfo("Uh Oh!",
+                        "Good news, you have $1,000,000 in paper money.\n"
+                        "Bad news, paper money means nothing in the apocalypse.")
     choice = simpledialog.askinteger("Choose wisely",
-                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
+                                     "You walk into a dusty plain, and you can see nothing.\n"
+                                     "You have your giant bag of money, however...You don't\n"
+                                     "know what's ahead.\n\nContinue (1)\nWalk Back (2)")
     if (choice == 1):
-        messagebox.showinfo("The End",
-                            "You chose right.  THE END")
+        messagebox.showinfo("Death by Radiation",
+                            "Cesium gets into your body. Due to the high amount of\n"
+                            "cesium, your muscles slowly deteriorate. You collapse.\n"
+                            "You die because of the intense heat and radiation.")
+        messagebox.showinfo("Ending",
+                            "You reached Ending 1 of 13 endings. Ending Type: Death")
 
     elif (choice == 2):
-        messagebox.showinfo("The End",
-                            "You chose ok.  THE END")
+        messagebox.showinfo("The Town",
+                            "You walk back from where you came, but you cannot see\n"
+                            "where you are. After a long walk you see a small town built\n"
+                            "out of steel and spare parts from a fighter jet. Houses are\n"
+                            "almost favela in their appearance.")
     else:
         choice2()
 
-################ Joseph's Functions #####################
-def choice3():
-    choice = simpledialog.askinteger("Choose wisely",
-                                     "This is the next part of the story.  Now you must choose 1 or 2 again.")
-    if (choice == 1):
-        messagebox.showinfo("The End",
-                            "You chose right.  THE END")
+def choice
 
-    elif (choice == 2):
-        messagebox.showinfo("The End",
-                            "You chose ok.  THE END")
-    else:
-        choice3()
-        
 ################ Main #####################
 intro()
 

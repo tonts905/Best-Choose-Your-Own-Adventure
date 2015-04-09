6e06f7ac6b9e5d5104f189826f9a5652373396d4
unknown <tonts905@CE105WD554105S.hillnet.ad.hsd.k12.or.us>
2015-04-09 15:27:51 -0700

    :)

--- template.py
+++ template.py
@@ -69,13 +69,7 @@ def choiceMoney1():
                                      "You have your giant bag of money, however...You don't\n"
                                      "know what's ahead.\n\nContinue (1)\nWalk Back (2)")
     if (choice == 1):
-        messagebox.showinfo("Death by Radiation",
-                            "Cesium gets into your body. Due to the high amount of\n"
-                            "cesium, your muscles slowly deteriorate. You collapse.\n"
-                            "You die because of the intense heat and radiation.")
-        messagebox.showinfo("Ending",
-                            "You reached Ending 1 of 13 endings. Ending Type: Death")
-        restart()
+        endingMoney1()
     elif (choice == 2):
         choiceMoney2()
     else:
@@ -98,8 +92,74 @@ def choiceMoney2():
          choiceWork()
      else:
          choiceMoney2()
-    
 
+def choiceSupplies():
+    choice = simpledialog.askinteger("Supplies",
+                                     "You say you are her for supplies. He asks you what is in\n"
+                                     "the bag over your shoulder. He searches your bad and\n"
+                                     "comments on how stupid you are for having paper money.\n"
+                                     "He asks if you are interested in working for him as a\n"
+                                     "'Peacekeeper', which is practically a bounty hunter.\n\n"
+                                     "Accept (1)\nDecline (2)")
+    if (choice == 1):
+        choicePeacekeeper()
+    if (choice == 2):
+        endingMoney2()
+    else:
+        choiceSupplies()
+    
+def choiceWork():
+    choice = simpledialog.askinteger("Work",
+                                     "You say you are looking for work. He offers you a position\n"
+                                     "as a 'Peackeeper', which is practically a bounty hunter.\n\n"
+                                     "Accept the offer (1)\nDecline the offer (2)")
+     if (choice == 1):
+        choicePeacekeeper()
+    if (choice == 2):
+        endingMoney2()
+    else:
+        choiceWork()
+
+def choicePeacekeeper():
+    messagebox.showinfo("Bounty",
+                        "You agree to the offer, then the sheriff hands you a paper\n"
+                        "and says 'Ask me for another paper when you're done.' You\n"
+                        "look at the paper with a small picture of the target, His crime\n"
+                        "and his bounty on him in gold coins.")
+    messagebox.showinfo("Revolver",
+                        "The Sheriff hands you a Revolver known for being used by\n"
+                        "Rangers. It is full loaded. He says 'careful...there's only six\n"
+                        "bullets in that. Bullets are on low supply around here. If you\n"
+                        "ever find some ammo lying around and want to sell it, go to\n"
+                        "Joseph the gun store owner.")
+
+def endingMoney1():
+    messagebox.showinfo("Death by Radiation",
+                        "Cesium gets into your body. Due to the high amount of\n"
+                        "cesium, your muscles slowly deteriorate. You collapse.\n"
+                        "You die because of the intense heat and radiation.")
+    messagebox.showinfo("Ending",
+                        "You reached Ending 1 of 13 endings. Ending Type: Death")
+    restart()
+
+def endingMoney2():
+    messagebox.showinfo("Decline",
+                        "You decline the offer. He says, 'Your loss.' You choose to stay\n"
+                        "in the town, working as a busser at the only bar. You live the\n"
+                        "rest of your boring life in this town.")
+    messagebox.showinfo("Ending",
+                        "You reached Ending 2 of 13 endings. Ending Type: Poor")
+    restart()
+
+def endingMoney3():
+    
+def endingMoney4():
+    
+def endingMoney5():
+    
+def endingMoney6():
+
+    
 ################ Main #####################
 intro()
 

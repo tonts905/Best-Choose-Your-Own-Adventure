bc19c4f3947f61a69a7a26663b389f4a91ef5b2c
unknown <tonts905@CE105WD554105S.hillnet.ad.hsd.k12.or.us>
2015-04-20 15:16:29 -0700

    Paper Money Branch - Done

--- template.py
+++ template.py
@@ -18,11 +18,12 @@ w.pack()
 
 def intro():
     """ Introductory Function -> starts the story going """
-    messagebox.showinfo("MLG APOCALYPSE SIMULATOR 2015", "\nYou are a survivor in the apocalypse in the year 2031."
-                        "\nWWIII has happened, resulting in world wide chemical, biological"
-                        "\nand nuclear war. You stand in front of a duffle bag of money and"
-                        "\na duffle bag of gold riches. Which do you take?")
-    choice = simpledialog.askinteger("Choose wisely","\nGold Riches (1)\nPaper Money (2).")
+    messagebox.showinfo("MLG APOCALYPSE SIMULATOR 2015",
+                        "You are a survivor of the apocalypse in the year 2031. WWIII\n"
+                        "has occured, resulting in worldwide chemical, biological\n"
+                        "and nuclear war. You stand in front of a duffle bag of money\n"
+                        "and a duffle bag of gold riches. Which do you take?")
+    choice = simpledialog.askinteger("Choose wisely","Gold Riches (1)\nPaper Money (2)")
     if choice == 1:
         choice1()
     elif choice == 2:
@@ -43,7 +44,7 @@ def restart():
          restart()
             
 ################ Joseph's Fuctions #####################
-def choice1():
+def choiceGold1():
     choice = simpledialog.askinteger("Choose wisely",
                                      "This is the next part of the story.  Now you must choose 1 or 2 again.")
     if (choice == 1):
@@ -63,13 +64,16 @@ def choice1():
 
 ################ Steven's Functions #####################
 def choiceMoney1():
-    messagebox.showinfo("Uh Oh!",
-                        "Good news, you have $1,000,000 in paper money.\n"
+    messagebox.showinfo("GG",
+                        "Good news, you have $1,000,000 in paper money.")
+    messagebox.showinfo("GG",
                         "Bad news, paper money means nothing in the apocalypse.")
+    messagebox.showinfo("Plains",
+                        "You walk into a dusty plain, and you can see nothing.\n"
+                        "You have your giant bag of money, however, you don't\n"
+                        "know what's ahead.")
     choice = simpledialog.askinteger("Choose wisely",
-                                     "You walk into a dusty plain, and you can see nothing.\n"
-                                     "You have your giant bag of money, however...You don't\n"
-                                     "know what's ahead.\n\nContinue (1)\nWalk Back (2)")
+                                     "Continue (1)\nWalk Back (2)")
     if (choice == 1):
         endingMoney1()
     elif (choice == 2):
@@ -83,8 +87,10 @@ def choiceMoney2():
                             "where you are. After a long walk you see a small town built\n"
                             "out of steel and spare parts from a fighter jet. Houses are\n"
                             "almost favela in their appearance.")
+     messagebox.showinfo("Sheriff",
+                            "You enter the town.")
      choice = simpledialog.askinteger("Sheriff",
-                                      "You enter the town. The town sheriff walks up to you and\n"
+                                      "The town sheriff walks up to you and\n"
                                       "asks you what you are doing around here.\n\n"
                                       "Say you are here for supplies. (1)\n"
                                       "Say you are looking for work. (2)")
@@ -96,10 +102,13 @@ def choiceMoney2():
          choiceMoney2()
 
 def choiceSupplies():
-    choice = simpledialog.askinteger("Supplies",
-                                     "You say you are here for supplies. He asks you what is in\n"
-                                     "the bag over your shoulder. He searches your bag and\n"
-                                     "comments on how stupid you are for having paper money.\n"
+    messagebox.showinfo("Supplies",
+                        "You say you are here for supplies. He asks you what is in\n"
+                        "the bag over your shoulder.")
+    messagebox.showinfo("Supplies",
+                        "He searches your bag and comments on how stupid you are\n"
+                        "for having paper money.")
+    choice = simpledialog.askinteger("Peacekeeper",
                                      "He asks if you are interested in working for him as a\n"
                                      "'Peacekeeper', which is practically a bounty hunter.\n\n"
                                      "Accept (1)\nDecline (2)")
@@ -124,31 +133,43 @@ def choiceWork():
 
 def choicePeacekeeper():
     messagebox.showinfo("Bounty",
-                        "You agree to the offer, then the sheriff hands you a paper\n"
-                        "and says 'Ask me for another paper when you're done.' You\n"
-                        "look at the paper with a small picture of the target, his crime\n"
-                        "and his bounty on him, in gold coins.")
+                        "You agree to the offer. The sheriff hands you a paper\n"
+                        "and says 'Ask me for another paper when you're done.'\n")
+    messagebox.showinfo("Bounty",
+                        "You look at the paper with a small picture of the target,\n"
+                        "his crime and his bounty on him, in gold coins.")
     messagebox.showinfo("Revolver",
-                        "The Sheriff hands you a revolver known for being used by\n"
-                        "Rangers. It is full loaded. He says 'Careful...there's only six\n"
-                        "bullets in that. Bullets are on low supply around here. If you\n"
-                        "ever find some ammo lying around and want to sell it, go to\n"
+                        "The Sheriff hands you a revolver known for being used\n"
+                        "by Rangers. It is full loaded.")
+    messagebox.showinfo("Revolver",
+                        "He warns 'Careful...there's only six bullets in that.\n"
+                        "Bullets are on low supply around here. If you ever find\n"
+                        "some ammo lying around and want to sell it, go to\n"
                         "Joseph the gun store owner.")
     messagebox.showinfo("Takeout",
-                        "You go to the house in the outskirts where\n"
-                        "it says he is, sneaking in through the back, crouching behind\n"
-                        "the counter. You pull our your gun, using the counter to help\n"
-                        "you aim. You shoot him once. He falls over and you walk towards\n"
-                        "his limp form, shooting him once more...eliminating your target.\n"
-                        "You search his body and find ammo...but it's not for your gun.\n"
-                        "You take his ring off and his necklace, as proof of your kill. You\n"
-                        "walk back to the town, slightly shaken up. You present the proof\n"
-                        "to the sheriff, who pays you one gold coin.")
+                        "You go to the house in the outskirts where it says\n"
+                        "he is. You sneak in through the back, crouching behind\n"
+                        "the counter.")
+    messagebox.showinfo("Takeout",
+                        "You pull out your gun, using the counter to help you\n"
+                        "aim. You shoot him once.")
+    messagebox.showinfo("Takeout",
+                        "He falls over. You walk towards his limp form, shooting\n"
+                        "him once more, eliminating your target. You search his\n"
+                        "body and find ammo, but it's not for your gun.")
+    messagebox.showinfo("Proof",
+                        "You take his ring off and his necklace, as proof of your\n"
+                        "kill.")
+    messagebox.showinfo("Proof",
+                        "You walk back to the town, slightly shaken up. You present\n"
+                        "the proof to the sheriff, who pays you one gold coin.")
+    messagebox.showinfo("Joseph",
+                        "You go to Joseph and he asks if you want to buy more\n"
+                        "rounds for the Peacekeeper revolver. You buy six more\n"
+                        "rounds, spending your gold coin.")
     choice = simpledialog.askinteger("Sell",
-                                     "You go to Joseph and he asks if you want to buy more\n"
-                                     "rounds for the Peacekeeper revolver. You buy six more\n"
-                                     "rounds, spending your gold coin. He asks if you have \n"
-                                     "unneeded ammo.\n\nSell your ammo (1)\nKeep your ammo (2)")
+                                     "He asks if you have unneeded ammo.\n\n"
+                                     "Sell your ammo (1)\nKeep your ammo (2)")
     if (choice == 1):
         choicePeacekeeper2()
     elif (choice == 2):
@@ -157,10 +178,12 @@ def choicePeacekeeper():
         choicePeacekeeper()
 
 def choicePeacekeeper2():
-    choice = simpledialog.askinteger("Next",
-                                     "You sell the rare sniper ammo, the .338 round. You obtain\n"
-                                     "10 coins from Joseph. He offers you a Tec-9 gun for sale.\n"
-                                     "\nPurchase the Tec-9 (1)\nReject the offer (2)")
+    messagebox.showinfo("Sell",
+                        "You sell the rare sniper ammo, the .338 round. You\n"
+                        "obtain 10 coins from Joseph.")
+    choice = simpledialog.askinteger("Tec-9",
+                                     "He offers you a Tec-9 gun for sale.\n\n"
+                                     "Purchase the Tec-9 (1)\nReject the offer (2)")
     if (choice == 1):
         choicePeacekeeper3()
     elif (choice == 2):
@@ -171,16 +194,22 @@ def choicePeacekeeper2():
 def choicePeacekeeper3():
     messagebox.showinfo("Tec-9",
                         "You buy the Tec-9 and two magazines for 10 coins.")
+    messagebox.showinfo("Mission",
+                        "You go to the sheriff for another bounty. He hands you\n"
+                        "multiple papers and says he needs you to take down a local\n"
+                        "gang in the area that is expanding in members.")
+    messagebox.showinfo("Mission",
+                        "They destroyed many towns with their 'Jet' (drug), which is\n"
+                        "extremely addictive. You see that the bounty is two gold\n"
+                        "bars, which is worth 200 gold coins.")
+    messagebox.showinfo("Mission",
+                        "You make your way towards the target location. You find a\n"
+                        "small complex at the site, in which you deduce is their base.")
     choice = simpledialog.askinteger("Mission",
-                                     "You go to the sheriff for another bounty. He hands you\n"
-                                     "multiple papers and says he needs you to take down a local\n"
-                                     "gang in the area that is expanding in members. They destroyed\n"
-                                     "many towns with their 'Jet' (drug), which is extremely\n"
-                                     "addictive. You see that the bounty is two gold bars, which is\n"
-                                     "worth 200 gold coins. You scout their hideout. Do you use\n"
-                                     "the bomb the sheriff gave you and take them out quickly, or\n"
-                                     "do you enter and terminate them silently?\n\nUse the C4 (1)\n"
-                                     "Silently kill each raider (2)")
+                                     "You scout their hideout. After a rough diagnosis, you realize\n"
+                                     "you have two options. Do you use the bomb the sheriff gave\n"
+                                     "you and take them out quickly, or do you enter and terminate\n"
+                                     "them silently?\n\nUse the C4 (1)\nSilently kill each raider (2)")
     if (choice == 1):
         endingMoney5()
     elif (choice == 2):
@@ -236,7 +265,7 @@ def endingMoney5():
                         "You activate the detonator (a makeshift garage door opener),\n"
                         "causing the house to explode a lot more violently than you\n"
                         "expected. Debris flies everywhere as the house is enveloped\n"
-                        "in engulfing flames. Youmust have struck a gas line...")
+                        "in engulfing flames. You must have struck a gas line...")
     messagebox.showinfo("Luxury",
                         "You go back to the town. The sheriff says, 'I could feel that\n"
                         "explosion from here! I thought you were killed...'")
@@ -262,7 +291,7 @@ def endingMoney6():
                         "You move to the living room and annihilate two with your revolver.\n"
                         "You hear several in the garage, sneaking in and hiding behind a bench.")
     messagebox.showinfo("Stealth",
-                        "When you are sure they cannot see you, you fire at the bench, your\n"
+                        "When you are sure they cannot see you, you fire at the bench. Your\n"
                         "bullets penetrate the bench and leave tennis ball sized holes in their\n"
                         "chests.")
     messagebox.showinfo("Stealth",
@@ -272,7 +301,7 @@ def endingMoney6():
     messagebox.showinfo("Stealth",
                         "You find a medical bot in the corner. You switch it to the bullet wound\n"
                         "setting, and it puts you to sleep.")
-    time.sleep(5)
+    time.sleep(2)
     messagebox.showinfo("Gold",
                         "You wake up two hours later. Fully healed, you walk back to the sheriff,\n"
                         "carry your loot with you in a sack. He has somehow confirmed your kills.")
